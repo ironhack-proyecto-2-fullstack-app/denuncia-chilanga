6b2c564477e94dbc91479167653960324d12d85e
Tomas <tomasfreire@gmail.com>
2019-04-16 20:36:10 -0500

    first commit

--- app.js
+++ app.js
@@ -54,5 +54,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+console.log("firs commit")
+
 
 module.exports = app;
