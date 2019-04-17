0643beaaf54690ffe3696c2ba7abec1b3cf10a23
Tomas <tomasfreire@gmail.com>
2019-04-16 22:15:39 -0500

    correct DB

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/proyecto-2-fullstack-app', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
