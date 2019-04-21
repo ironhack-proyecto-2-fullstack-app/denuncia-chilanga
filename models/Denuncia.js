3dc5a374cd2cafb049418231b312ffb5fd5461d6
Tomas <tomasfreire@gmail.com>
2019-04-20 19:11:02 -0500

    se agrego el mapa al formulario de denuncias

--- models/Denuncia.js
+++ models/Denuncia.js
@@ -3,38 +3,45 @@ const Schema = mongoose.Schema;
 
 const denunciaSchema = new Schema(
   {
-user: {      
-  type: Schema.Types.ObjectId,
-  required: false,
-  ref: "User"},
-folio: {
-  type: Number, 
-  required: false 
-  ,unique:true
-},
-titulo: {
-  type:String, 
-  requires:true},
-descripcion:String,
-ubicacion:{
-  //required:true,
-  type: {
-    type: String,
-    default: "Point"},
-  address: {
-    type: String},
-  colonia:{
-    type:String},
-  coordenadas: {
-    type: [Number]}},
-images:{type:[String]},
-estatus:{type:String, 
-  required: true, 
-  enum:["Abierta","Cerrada","En Proceso"],
-  default:"Abierta"},
-categoria:{  type: Schema.Types.ObjectId,
-  required: true,
-  ref: "Categoria"}
+    user: {
+      type: Schema.Types.ObjectId,
+      required: false,
+      ref: "User"
+    },
+    folio: {
+      type: Number,
+      required: false,
+      unique: true
+    },
+    titulo: {
+      type: String,
+      requires: true
+    },
+    descripcion: String,
+    ubicacion: {
+      //required:true,
+      type: {
+        type: String,
+        default: "Point"
+      },
+      address: {
+        type: String
+      },
+      colonia: {
+        type: String
+      },
+      coordenadas: {
+        type: [Number]
+      }
+    },
+    images: { type: [String] },
+    estatus: {
+      type: String,
+      required: true,
+      enum: ["Abierta", "Cerrada", "En Proceso"],
+      default: "Abierta"
+    },
+    categoria: { type: Schema.Types.ObjectId, required: true, ref: "Categoria" }
   },
   { timestamps: true }
 );
